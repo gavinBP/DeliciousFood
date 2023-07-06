aa598b1a6819701ed8c59488a9948c3fec64854f
gavinBP <gavin.briggs_perez@protonmail.com>
2023-07-05 19:13:23 -0500

    set up connection to azure database

--- src/server/server.js
+++ src/server/server.js
@@ -2,6 +2,55 @@ const express = require('express');
 const path = require('path');
 
 const app = express();
+const sql = require('mssql');
+const { connect } = require('http2');
+
+const config = {
+  server: 'fooditemssqldatabase.database.windows.net',
+  port: 1433,
+  user: 'fooditemsserverlogin',
+  password: 'foodItemsPassword10',
+  database: 'FoodItemsDB',
+  options: {
+    encrypt: true, // if your server requires encryption
+  },
+};
+
+let connected = false;
+
+async function azureConnectOpen() {
+    try {
+      await sql.connect(config);
+      console.log('Connected!');
+      connected = true;
+  
+      // Place your database queries or operations here
+  
+      // Close the database connection when finished
+    //   await sql.close();
+    } catch (err) {
+      console.error('Error connecting to the database:', err);
+    }
+  }
+
+  async function azureConnectClose() {
+    try {
+        if(connected) {
+            await sql.close();
+            console.log('Connetcion Closed');
+            connected = false;
+        }
+        else {
+            console.log('already not connected');
+        }
+    }
+    catch (err) {
+        console.error('Error in closing connection to azure database: ', err);
+    }
+  }
+  azureConnectOpen();
+  setTimeout(azureConnectClose, 5000);
+
 app.use(express.json());
 
 
